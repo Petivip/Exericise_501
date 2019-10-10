5f13ff0f2ed2bcbb22f82841318a88495f538cd1
petivip <petivip@gmail.com>
2019-10-10 09:44:20 -0400

    501

---
+++ src/main/java/com/example/demo/Exericise501Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exericise501Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Exericise501Application.class, args);
+    }
+
+}
